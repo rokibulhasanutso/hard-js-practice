e60d2b5013e268bf058c144caf2b53796ef4986b
rokibulhasan <rokibulhasanutsoo@gmail.com>
2023-06-13 11:47:05 +0600

    Write Instrauction

---
+++ 00_start.js
@@ -0,0 +1,22 @@
+/*
+*   Hii,
+*   My name is Rokibul Hasan Utso.
+*   I'm JavaScript Developer.
+*   I'm from Bangladesh.
+*   
+*   
+*   ## Description
+*
+*   Since, I am still study going on JavaScript. So, I learn to share my code on Github. 
+*   And I develop my learning and building strong knowladge.
+*   So, I think that share my knowladge every body.
+*   
+*   
+*   ## Sharing Topics of Javascript 
+*   
+*   01. Console
+*   02. Variable
+*   03. 
+*   
+*   
+*/
