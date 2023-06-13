421558dfe5aec831e7a99b38cced962a6a7885b5
rokibulhasan <rokibulhasanutsoo@gmail.com>
2023-06-13 10:56:08 +0600

    manage

--- 01.js
+++
@@ -1,3 +0,0 @@
-// practice file 01.js
-
-console.log( 'Hello Wold..... I\'m start to hard practice JavaScript' );
---
+++ 01_console.js
@@ -0,0 +1,3 @@
+// practice file 01.js
+
+console.log( 'Hello Wold..... I\'m start to hard practice JavaScript' );
