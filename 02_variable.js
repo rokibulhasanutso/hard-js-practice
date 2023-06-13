7205bd177c58893ee7998da17e77e7fdf980feb0
rokibulhasan <rokibulhasanutsoo@gmail.com>
2023-06-13 12:19:10 +0600

    how to declare variable name with numbers

---
+++ 02_variable.js
@@ -0,0 +1,17 @@
+// variable introduction
+
+// What is variable ?
+// Variable is visualization storing box. Variable can store some data or information.
+
+// Syntex
+var myName = 'Rokibul Hasan' //
+
+// Variable name declaration rules
+// 1. Variable declaration can not start with a number 
+
+// var 123 = "First name"; // wrong
+// var 123ABC = "First name"; // wrong
+// var 123$ = "First name"; // wrong
+
+// right way |start with string and after use number|
+var abc123 = "First name";
