d39a83ebb0d8cd31029744b26594f94c1d3ef74a
rokibulhasan <rokibulhasanutsoo@gmail.com>
2023-06-13 12:57:45 +0600

    Write namming convention

--- 02_variable.js
+++ 02_variable.js
@@ -15,3 +15,30 @@ var myName = 'Rokibul Hasan' //
 
 // right way |start with string and after use number|
 var abc123 = "First name";
+
+// 2. Others way you can use Dolar($) and underscore(_) sign
+var $abc123 = "First name";
+var _abc123 = "First name";
+
+// 3. JavaScipt is a case sensitive language
+var ABC = "First name";
+var abc = "firstname";
+// Note: ABC and abc not same. There are two different Variable name
+
+// 4. And also others way you can not declare a variable
+// var #abc = "First name";
+// var #123 = "First name";
+// var _#abc = "First name";
+// var $#123 = "First name";
+// var @abc = "First name";
+// var %abc = "First name";
+// var /123 = "First name";
+// var *123 = "First name";
+// var -123 = "First name";
+// var +123 = "First name";
+
+// Note: I suggest that variable name decleare with alphabate
+var yourname = "Humyrah";
+var your_name = "Humyrah";
+// better word convention
+var yourName = "Monalisa";
